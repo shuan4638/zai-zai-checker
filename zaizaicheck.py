37018facaf4955322cdb2ab75cf698e77921c62a
Shuan Chen <43088083+shuan4638@users.noreply.github.com>
2022-07-31 17:16:30 +0900

    Update zaizaicheck.py

--- zaizaicheck.py
+++ zaizaicheck.py
@@ -6,9 +6,9 @@ headers = {"Authorization": "Bearer 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"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
-    
-def multimask_predictions(txt, outputs, check_vocabs = ["再", "在"]):
+
+
+def multimask_predictions(txt, outputs, check_vocabs):
     all_outputs = {}
     for i, i_output in enumerate(outputs):
         for output in i_output:
@@ -25,22 +25,21 @@ def multimask_predictions(txt, outputs, check_vocabs = ["再", "在"]):
             pred_cnt += 1
         corrected_txt += word
     return corrected_txt
-    
+
 def main(check_vocabs):
-    txt = input("打中文: ") # 我在說一次
+    txt = input("打中文: ")  # user input
     txt_mask = txt
     for word in check_vocabs:
         txt_mask = txt_mask.replace(word, '[MASK]')
 
     output = query({ "inputs": txt_mask, })
-    if txt_mask.count('[MASK]') <= 1: # only one zai detected 
+    if txt_mask.count('[MASK]') <= 1:  # only one word is detected
         corrected_txt = output[0]['sequence'].replace(" ", "")
-    else:
-        corrected_txt = multimask_predictions(txt, output)
+    else:  # more than one word are masked
+        corrected_txt = multimask_predictions(txt, output, check_vocabs)
     print(txt + " -> " + corrected_txt)
-    return 
+    return
 
 if __name__ == '__main__':
     zai_zai = ['在', '再']
     main(zai_zai)
-    
